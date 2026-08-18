8def2e84213686e1945591dfc78a6d5682f78cb6
Salem Saleh <salem.saleh.tech@gmail.com>
2026-08-18 12:51:13 +0300

    building table without component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-
+const data =[
+  {fullName: "salem", age: 17, email: "salem@example.com", class: "10th Grade"},
+  {fullName: "ali", age: 17, email: "ali@example.com", class: "10th Grade"},
+  {fullName: "ahmed", age: 17, email: "ahmed@example.com", class: "10th Grade"}
+]
 function App() {
 
   return (
     <>
-    <h1>Salem Saleh</h1>
+    <table>
+      <thead>
+        <th>Full Name</th>
+        <th>Age</th>
+        <th>Email</th>
+        <th>Class</th>
+      </thead>
+      <tbody>
+        {data.map((item)=>{
+          return(
+            <tr>
+              <td>{item.fullName}</td>
+              <td>{item.age}</td>
+              <td>{item.email}</td>
+              <td>{item.class}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+
+    </table>
     </>
   )
 }
